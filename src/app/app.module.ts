2dd0f5bcb5254be7f3b9efd02bcadb9148434691
Neill Barnard <>
2020-03-18 14:59:07 +0200

    Made changes ot assist making the website responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/choose-technology/choose-technology.component.ts
+++ src/app/choose-technology/choose-technology.component.ts
@@ -9,8 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class ChooseTechnologyComponent implements OnInit {
 
   constructor() { }
-
-  ngOnInit(): void {
+   allLanguagess = [
+     {id:"flutter",img:"assets/fluttericon.png"},
+     {id:"swift",img:"assets/swifticon.png"},
+     {id:"node",img:"assets/nodeicon.png"},
+     {id:"angular",img:"assets/angularicon.svg"},
+     {id:"kony",img:"assets/konyicon.png"},
+     {id:"wordpress",img:"assets/wordpressicon.png"}];
+     status : boolean = false;
+   ngOnInit(): void {
+  
+  
   }
+
+ test(event){
+  this.status = true
+ if (this.allLanguagess.indexOf(event) > 0) {
+  this.allLanguagess.splice(this.allLanguagess.indexOf(event), 1);
+  this.allLanguagess.unshift(event);
+}
+  console.log(this.allLanguagess);
+ }
 
 }
