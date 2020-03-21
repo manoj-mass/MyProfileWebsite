0b42cc77e21e9c81beeba7c92cfb019ba223058a
Manoj Masakorala <manoj.masakorala@pearson.com>
2020-03-21 21:37:48 +0530

    minor bug fixes

--- src/app/contact-header/contact-header.component.ts
+++ src/app/contact-header/contact-header.component.ts
@@ -7,21 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    window.addEventListener('resize', this.getWindowSize);
+  }
 
   ngOnInit(): void {
+    this.getWindowSize();
     this.fadeOutAnimation();
   }
 
+  getWindowSize = () => {
+    const elPush: any = document.querySelector('.move-right');
+    const elWave: any = document.querySelector('.waving_gif');
+    const elButton: any = document.querySelector('.annimatedButton');
+    elButton.style.left = document.documentElement.clientWidth - 180 + 'px';
+    elPush.style.left = document.documentElement.clientWidth - 280 + 'px';
+    elWave.style.left = document.documentElement.clientWidth - 260 + 'px';
+  }
   fadeOutAnimation = () => {
     const elPush: any = document.querySelector('.move-right');
     const elWave: any = document.querySelector('.waving_gif');
+    const elButton: any = document.querySelector('.annimatedButton');
     if (elPush && elWave) {
       elPush.addEventListener('animationend', () => {
         elPush.style.transition = '0.8s';
         elPush.style.opacity = 0;
         elWave.style.opacity = 1;
         elWave.style.transition = '0.5s';
+        elButton.style.animation = 'none';
+        elPush.style.animation = 'none';
+        elWave.style.animation = 'none';
       }, false);
     }
   }
