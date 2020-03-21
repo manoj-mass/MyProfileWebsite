c76be86b2a4d712c0f4fc575f097e72866ac0004
Neill Barnard <>
2020-03-21 15:09:58 +0200

    changes before animation integration

--- src/app/choose-technology/choose-technology.component.ts
+++ src/app/choose-technology/choose-technology.component.ts
@@ -16,19 +16,44 @@ export class ChooseTechnologyComponent implements OnInit {
      {id:"angular",img:"assets/angularicon.svg"},
      {id:"kony",img:"assets/konyicon.png"},
      {id:"wordpress",img:"assets/wordpressicon.png"}];
+    allButton = {id:"all",img:"assets/ALL.svg"};
      status : boolean = false;
-   ngOnInit(): void {
-  
+     allButtonPressed : boolean = false;
+     mobileView: boolean = false;
+   
+   
+     ngOnInit(): void {
+      if(window.innerWidth < 941){
+        this.mobileView = true;
+      }else{
+        this.mobileView = false;
+      }
+    
   
   }
 
- test(event){
+ MovingLanguageIcon(event){
+   console.log(event);
+   
+ if(event.id != "all" ){
+   this.allButtonPressed = false;
   this.status = true
- if (this.allLanguagess.indexOf(event) > 0) {
-  this.allLanguagess.splice(this.allLanguagess.indexOf(event), 1);
-  this.allLanguagess.unshift(event);
+//  if (this.allLanguagess.indexOf(event) > 0) {
+//   this.allLanguagess.splice(this.allLanguagess.indexOf(event), 1);
+//   this.allLanguagess.unshift(event);
+// }
+ }else{
+  this.status = false;
+  this.allButtonPressed = true;
+ }
 }
-  console.log(this.allLanguagess);
+ onResize(event){
+  if(event < 941){
+    this.mobileView = true;
+  }else{
+    this.mobileView = false;
+  }
+
  }
 
 }
