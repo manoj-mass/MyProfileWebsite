071a03e9d9e8c1d2505c4b1ea715f77ae96c59fb
Manoj Masakorala <manoj.masakorala@pearson.com>
2020-03-22 13:38:05 +0530

    change in circle background color

--- src/app/choose-technology/choose-technology.component.ts
+++ src/app/choose-technology/choose-technology.component.ts
@@ -10,42 +10,42 @@ export class ChooseTechnologyComponent implements OnInit {
 
   constructor() { }
    allLanguagess = [
-     {id:"flutter",img:"assets/fluttericon.png"},
-     {id:"swift",img:"assets/swifticon.png"},
-     {id:"node",img:"assets/nodeicon.png"},
-     {id:"angular",img:"assets/angularicon.svg"},
-     {id:"kony",img:"assets/konyicon.png"},
-     {id:"wordpress",img:"assets/wordpressicon.png"}];
+     {id:"flutter",img:"assets/fluttericon.png", active: false},
+     {id:"swift",img:"assets/swifticon.png", active: false},
+     {id:"node",img:"assets/nodeicon.png", active: false},
+     {id:"angular",img:"assets/angularicon.svg", active: false},
+     {id:"kony",img:"assets/konyicon.png", active: false},
+     {id:"wordpress",img:"assets/wordpressicon.png", active: false}];
     allButton = {id:"all",img:"assets/ALL.svg"};
      status : boolean = false;
      allButtonPressed : boolean = false;
      mobileView: boolean = false;
-   
-   
+
+
      ngOnInit(): void {
       if(window.innerWidth < 941){
         this.mobileView = true;
       }else{
         this.mobileView = false;
       }
-    
-  
+
+
   }
 
- MovingLanguageIcon(event){
-   console.log(event);
-   
- if(event.id != "all" ){
-   this.allButtonPressed = false;
-  this.status = true
+ MovingLanguageIcon(event, data){
+   console.log(event , data);
+   data.active = !data.active;
+//  if(event.id != "all" ){
+//    this.allButtonPressed = false;
+//   this.status = true
 //  if (this.allLanguagess.indexOf(event) > 0) {
 //   this.allLanguagess.splice(this.allLanguagess.indexOf(event), 1);
 //   this.allLanguagess.unshift(event);
 // }
- }else{
-  this.status = false;
-  this.allButtonPressed = true;
- }
+//  }else{
+//   this.status = false;
+//   this.allButtonPressed = true;
+//  }
 }
  onResize(event){
   if(event < 941){
